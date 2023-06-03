52eeb89d3201e96e09b9e7616a872fea9c11fce9
Butterscotch Stallion <mistborn+github@fastmail.com>
2023-06-03 17:37:57 -0400

    Removes unused styles, fixes up test some more

--- src/components/experience/experience-display.test.tsx
+++ src/components/experience/experience-display.test.tsx
@@ -1,23 +1,15 @@
 /**
  * @jest-environment jsdom
  */
-import { screen } from '@testing-library/react';
-import { createRoot } from 'react-dom/client';
-import { act } from 'react-dom/test-utils';
+import { render, screen } from '@testing-library/react';
 import { ExperienceDisplay } from './ExperienceDisplay';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('ExperienceDisplay', function () {
   test('should display experience', function () {
     const totalXP = '500';
-    act(() => {
-      const container: HTMLElement | null = document.getElementById('app');
-      if (container) {
-        const root = createRoot(container);
-        root.render(<ExperienceDisplay totalExperience={totalXP} />);
-        expect(screen.getByTestId('ExperienceDisplay')).toHaveTextContent(
-          totalXP
-        );
-      }
-    });
+    render(<ExperienceDisplay totalExperience={totalXP} />);
+    const testEl: HTMLElement = screen.getByTestId('ExperienceDisplay');
+    expect(testEl).toHaveTextContent(totalXP);
   });
 });
