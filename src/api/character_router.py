71444b5c7d679dc17f843ad7b1b27a3b78bb1157
Butterscotch Stallion <mistborn+github@fastmail.com>
2023-06-10 10:21:55 -0400

    Adds new character route

--- src/api/character_router.py
+++ src/api/character_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 import logging as log
 from sqlalchemy.orm import Session
 import schemas
+from character import get_character_by_guid
 from models import Characters, db
 
 log.basicConfig(level=log.INFO)
@@ -10,9 +11,9 @@ character_router = APIRouter()
 
 @character_router.get("/api/characters", response_model=list[schemas.CharactersBase])
 def get_characters(skip: int = 0, limit: int = 100, session: Session = Depends(db.get_session)):
-    return Characters.get_users(session, skip=skip, limit=limit)
+    return Characters.get_characters(session, skip=skip, limit=limit)
 
 
 @character_router.get("/api/characters/<guid>", response_model=list[schemas.CharactersBase])
-def get_character(session: Session = Depends(db.get_session)):
-    return Characters.get_users(session)
+def get_character(guid, session: Session = Depends(db.get_session)):
+    return get_character_by_guid(db, guid)
