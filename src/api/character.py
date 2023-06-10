99275dd93c5da17a61ed59e4e86c60149065c657
Butterscotch Stallion <mistborn+github@fastmail.com>
2023-06-10 18:40:35 -0400

    Adds route for updating

--- src/api/character.py
+++ src/api/character.py
@@ -4,3 +4,11 @@ import models
 
 def get_character_by_guid(db: Session, guid: str):
     return db.query(models.Character).filter(models.Character.guid == guid).first()
+
+
+def get_characters(db: Session, limit: int, skip: int):
+    return db.query(models.Characters).offset(skip).limit(limit).all()
+
+
+def update_character_by_guid(db: Session, guid: str, user: models.Characters):
+    return db.query(models.Characters).update(user)
--- src/api/character_router.py
+++ src/api/character_router.py
@@ -3,7 +3,7 @@ import logging as log
 from sqlalchemy.orm import Session
 import schemas
 from character import get_character_by_guid
-from models import Characters, db
+from models import db
 
 log.basicConfig(level=log.INFO)
 character_router = APIRouter()
@@ -11,9 +11,14 @@ character_router = APIRouter()
 
 @character_router.get("/api/characters", response_model=list[schemas.CharactersBase])
 def get_characters(skip: int = 0, limit: int = 100, session: Session = Depends(db.get_session)):
-    return Characters.get_characters(session, skip=skip, limit=limit)
+    return get_characters(session, skip=skip, limit=limit)
 
 
 @character_router.get("/api/characters/<guid>", response_model=list[schemas.CharactersBase])
 def get_character(guid, session: Session = Depends(db.get_session)):
-    return get_character_by_guid(db, guid)
+    return get_character_by_guid(session, guid)
+
+
+@character_router.post("/api/characters/<guid>", response_model=list[schemas.CharactersBase])
+def get_character(guid, session: Session = Depends(db.get_session)):
+    return update_character_by_guid(session, guid)
